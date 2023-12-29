e2ce816067630fd7598a785dd43a2525cbc2b227
Mostafa Elkhouly <mostafa.mohd96@hotmail.co.uk>
2023-12-29 23:55:33 +0200

    Build a Complete eCommerce Application

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,27 @@
+﻿using eTickets.Data.Base;
+using eTickets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService : IEntityBaseRepository<Actor>
+    {
+        //IEnumerable<Actor> GetAll();
+
+        //Task<IEnumerable<Actor>> GetAllAsync();
+
+        //Task<Actor> GetByIdAsync(int id);
+
+        //Task AddAsync(Actor actor);
+
+        //Task<Actor> UpdateAsync(int id, Actor newActor);
+
+        //Task DeleteAsync(int id);
+
+
+
+    }
+}
